f22fadf52e68eab6bbca512438d8fd34ccc073e0
Валентина Андреева <andre8159@gmail.com>
2023-08-16 13:56:24 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98; // вес
+        double height = 1.87; //рост
+        int index = service.calculate(weight, height); //расчет
+
+        System.out.println("Индекс массы тела" + index);
+
+    }
+}
